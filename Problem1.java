911986da4bc504cf9a21b79665a11675b2179784
jbmdevelopment <jbarreramartinez100@student.hbuhsd.edu>
2024-09-09 11:20:05 -0700

    aaa

--- Problem1.java
+++ Problem1.java
@@ -1,1 +1,22 @@
-
+public class Problem1 {
+    public static void main(String[] args) {
+        // what the hell is this code lol
+        int value = 24;
+        value++;
+        System.out.println("Number is now " + value);
+        value++;
+        System.out.println("Number is now " + value);
+        value++;
+        System.out.println("Number is now " + value);
+        value++;
+        System.out.println("Number is now " + value);
+        value--;
+        System.out.println("Number is now " + value);
+        value--;
+        System.out.println("Number is now " + value);
+        value--;
+        System.out.println("Number is now " + value);
+        value--;
+        System.out.println("Number is now " + value);
+    }
+}
