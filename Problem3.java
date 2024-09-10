a7f9bd416acc473dcf19a14a24858e51a44104a6
jbmdevelopment <jbarreramartinez100@student.hbuhsd.edu>
2024-09-10 11:05:33 -0700

    commit3

--- Problem3.java
+++ Problem3.java
@@ -1,1 +1,13 @@
-
+public class Problem3 {
+    public static void main(String[] args) {
+        int quadDigit = 1234;
+        int firstDigit = (quadDigit / 1000) % 10;
+        int midDigit = (quadDigit / 100) % 10;
+        int midMidDigit = (quadDigit / 10) % 10;
+        int lastDigit = quadDigit % 10;
+        System.out.println(lastDigit);
+        System.out.println(midMidDigit);
+        System.out.println(midDigit);
+        System.out.println(firstDigit);
+    }
+}
