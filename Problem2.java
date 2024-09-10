4c79d5ec3677d6b7a51aa94144c77d3733c72f1d
jbmdevelopment <jbarreramartinez100@student.hbuhsd.edu>
2024-09-10 10:47:32 -0700

    commit3

--- Problem2.java
+++ Problem2.java
@@ -1,1 +1,11 @@
-
+public class Problem2 {
+    public static void main(String[] args) {
+        int threeDigit = 456;
+        int firstDigit = (threeDigit / 100) % 10;
+        int midDigit = (threeDigit / 10) % 10;
+        int lasttDigit = threeDigit % 10;
+        System.out.println(firstDigit);
+        System.out.println(midDigit);
+        System.out.println(lasttDigit);
+    }
+}
